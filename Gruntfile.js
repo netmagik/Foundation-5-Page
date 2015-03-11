2417736af829c2f7a4d12f67d5b8efc8f7848c57
Irina <irina@netmagik.com>
2015-03-11 11:01:16 -0400

    Start of project

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,32 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: 'scss/*.scss',
+        tasks: ['sass'],
+        options : {
+          spawn : false,
+        }
+      },
+
+      images : {
+        files : ['images/src/*.{png,jpg,gif}'],
+        tasks : ['newer:imagemin']
+      }, // watch images added to src folder
+
+    },
+
+
     sass: {
       options: {
         includePaths: ['bower_components/foundation/scss']
       },
       dist: {
         options: {
-          outputStyle: 'compressed'
+          outputStyle: 'expanded'
         },
         files: {
           'css/app.css': 'scss/app.scss'
@@ -16,19 +35,41 @@ module.exports = function(grunt) {
       }
     },
 
-    watch: {
-      grunt: { files: ['Gruntfile.js'] },
+    imagemin : {
+      dynamic : {
+        files : [{
+          expand : true, // Enable dynamic expansion
+          cwd : 'images/src/', // source images (not compressed)
+          src : ['**/*.{png,jpg,gif}'], // Actual patterns to match
+          dest : 'images/dist/' // Destination of compressed files
+        }]
+      }
+    }, //end imagemin
 
-      sass: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass']
+
+    browserSync : {
+      dev : {
+        bsFiles : {
+          src : ['css/*.css', 'images/*.*', 'js/*.js', '*.html']
+        },
+        options : {
+          server : {
+            baseDir : "./"
+
+          },
+          watchTask : true // < VERY important
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-browser-sync');
 
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
+//  grunt.registerTask('build', ['sass']);
+  //grunt.registerTask('default', ['build','watch']);
+  grunt.registerTask('default', ["browserSync", "watch"]);
 }
